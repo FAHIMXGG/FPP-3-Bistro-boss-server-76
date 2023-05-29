522c4a85a2544b74e324492588eb80a527b8c66d
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-05-29 23:57:26 +0600

    card

--- index.js
+++ index.js
@@ -39,6 +39,18 @@ async function run() {
     })
 
     // cart collections
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+
     app.post('/carts', async (req, res) => {
       const item = req.body;
       console.log(item);
